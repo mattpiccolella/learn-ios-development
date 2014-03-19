7bb0d184866e7c2da944abb8543df3a6a673d403
mjp2220 <mjp2220@columbia.edu>
2014-03-19 14:37:04 -0400

    Added URL calls, works to show basic repo names

--- Nerdfeed/Nerdfeed/BNRCoursesViewController.m
+++ Nerdfeed/Nerdfeed/BNRCoursesViewController.m
@@ -11,17 +11,29 @@
 @interface BNRCoursesViewController ()
 
 @property (nonatomic) NSURLSession *session;
+@property (nonatomic, copy) NSMutableArray *repos;
 
 @end
 
 @implementation BNRCoursesViewController
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 0;
+    return [self.repos count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return nil;
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
+ 
+    NSDictionary *repo = self.repos[indexPath.row];
+    cell.textLabel.text = repo[@"name"];
+    
+    return cell;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"UITableViewCell"];
 }
 
 - (instancetype)initWithStyle:(UITableViewStyle)style {
@@ -31,8 +43,28 @@
         
         NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
         _session = [NSURLSession sessionWithConfiguration:config delegate:nil delegateQueue:nil];
+        
+        [self fetchFeed];
     }
     return self;
+}
+
+- (void)fetchFeed {
+    NSString *requestString = @"https://api.github.com/users/mjp2220/repos";
+    NSURL *url = [NSURL URLWithString:requestString];
+    NSURLRequest *req = [NSURLRequest requestWithURL:url];
+    NSMutableArray *myRepos = [[NSMutableArray alloc] init];
+    NSURLSessionDataTask *dataTask = [self.session dataTaskWithRequest:req completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSDictionary *jsonObject = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+        for (NSDictionary *myData in jsonObject) {
+            [myRepos addObject:myData];
+        }
+        self.repos = myRepos;
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.tableView reloadData];
+        });
+    }];
+    [dataTask resume];
 }
 
 @end
