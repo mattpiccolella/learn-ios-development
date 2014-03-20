acf8380393f16d075a6df77b4888c79622df6bda
mjp2220 <mjp2220@columbia.edu>
2014-03-20 01:35:28 -0400

    Working with the basics of storyboards

---
+++ Colorboard/Colorboard/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Colorboard
+//
+//  Created by Matt on 3/20/14.
+//  Copyright (c) 2014 Matthew Piccolella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
